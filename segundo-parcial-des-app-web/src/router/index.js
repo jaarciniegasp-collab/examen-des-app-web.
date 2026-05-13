70c6f17aaedf8cff01eba197933a63aefe276e94
Juan Jaime <jsjaimeq@.edu.co>
2026-05-13 16:11:55 -0500

    terminacion productos

--- segundo-parcial-des-app-web/src/router/index.js
+++ segundo-parcial-des-app-web/src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import ProductView from '../views/ProductView.vue'
+import Viveres from '@/components/Viveres.vue'
+import Licor from '@/components/Licor.vue'
+import Lacteo from '@/components/Lacteo.vue'
+import Dulces from '@/components/Dulces.vue'
+import Cuidado from '@/components/Cuidado.vue'
 
 const routes = [
   {
@@ -38,7 +43,33 @@ const routes = [
     meta: {
       hideNavbar: true
     }
+  },
+  {
+    path:'/viveres',
+    name:'viveres',
+    component:Viveres,
+  },
+  {
+    path:'/licores',
+    name:'licores',
+    component:Licor,
+  },
+  {
+    path:'/lacteos',
+    name:'lacteos',
+    component:Lacteo,
+  },
+  {
+    path:'/dulces',
+    name:'dulces',
+    component:Dulces,
+  },
+  {
+    path:'/cuidado',
+    name:'cuidado',
+    component:Cuidado,
   }
+  
 ]
 
 const router = createRouter({

1b44a924ee3cbcc679380c9f781545bd4cf46af6
Jorge Arciniegas <jaarciniegasp@ufpso.edu.co>
2026-06-14 19:18:53 -0500

    fix: corrige bug en login

--- segundo-parcial-des-app-web/src/router/index.js
+++ segundo-parcial-des-app-web/src/router/index.js
@@ -1,23 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import LoginView from '@/views/LoginView.vue'
 import DashboardView from '@/views/DashboardView.vue'
 import ProductListView from '@/views/ProductListView.vue'
 import UserListView from '@/views/UserListView.vue'
 
 const routes = [
-  { path: '/', name: 'Login', component: LoginView },
+  {
+    path: '/',
+    name: 'Login',
+    component: LoginView
+  },
+
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: DashboardView,
     meta: { requiresAuth: true }
   },
+
   {
     path: '/products',
     name: 'ProductList',
     component: ProductListView,
     meta: { requiresAuth: true }
   },
+
   {
     path: '/users',
     name: 'UserList',
@@ -31,14 +39,13 @@ const router = createRouter({
   routes
 })
 
-// Navigation guards
 router.beforeEach((to, from, next) => {
-  const logueado = localStorage.getItem('token')
+  const token = localStorage.getItem('token')
 
-  if (to.meta.requiresAuth && !logueado) {
-    next({ name: 'Login' })
-  } else if (to.name === 'Login' && logueado) {
-    next({ name: 'Dashboard' })
+  if (to.meta.requiresAuth && !token) {
+    next('/')
+  } else if (to.path === '/' && token) {
+    next('/dashboard')
   } else {
     next()
   }
